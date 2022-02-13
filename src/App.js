f9c091f330af300b65c7de4903831e7c37957712
AdrijanaNj <adrijananjezic@gmail.com>
2022-02-13 21:34:14 +0100

    added some styling and lincs to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">Adrijana's Dictionary</header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary defaultKeyword="fire" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Adrijana Njezic</small>
+          <small>
+            {" "}
+            This project was coded by <span>Adrijana Njezic</span> and is{" "}
+            <a
+              href="https://github.com/AdrijanaNj/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://boring-hoover-76b98f.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
